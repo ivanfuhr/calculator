ba505e5185646e7e282ab2b5e13b7741206ff697
Ivan Führ <dev.ivanfuhr@gmail.com>
2021-05-17 21:04:17 -0300

    Projeto

--- README.md
+++ README.md
@@ -12,31 +12,27 @@
  - [ ] Fazer um post do LinkedIn
 
 # Requisitos
-- [ ] O usuário pode ver uma tela mostrando o número atual inserido ou o
+- [X] O usuário pode ver uma tela mostrando o número atual inserido ou o
 resultado da última operação.
-- [ ] O usuário pode ver um painel de entrada contendo botões para os dígitos 0-9, 
+- [X] O usuário pode ver um painel de entrada contendo botões para os dígitos 0-9, 
 operações - '+', '-', '/' e '=', um botão 'C' (para limpar) e um 'AC'
 botão (para limpar tudo).
--  [ ] O usuário pode inserir números como sequências de até 8 dígitos clicando em
-dígitos no teclado de entrada. A entrada de qualquer dígito superior a 8 será ignorada.
-- [ ] O usuário pode clicar em um botão de operação para exibir o resultado dessa
+- [X] O usuário pode clicar em um botão de operação para exibir o resultado dessa
 operação em:
     * o resultado da operação anterior e o último número inserido OU
     * os dois últimos números inseridos OU
     * o último número inserido
-- [ ] O usuário pode clicar no botão 'C' para limpar o último número ou o último
+- [X] O usuário pode clicar no botão 'C' para limpar o último número ou o último
 Operação. Se a última entrada do usuário foi uma operação, o display será
 atualizado para o valor que o precedeu.
-- [ ] O usuário pode clicar no botão 'AC' para limpar todas as áreas de trabalho internas e
+- [X] O usuário pode clicar no botão 'AC' para limpar todas as áreas de trabalho internas e
 para definir a exibição para 0.
-- [ ] O usuário pode ver 'ERR' exibido se qualquer operação exceder o 
-Máximo de 8 dígitos.
 
 # Relatório
-...
+Projeto simples, mas creio que seja de grande relevancia para programadores iniciantes desenvolver tal projeto, tive um problema muito simples e acabei ignorando, isso me fez aprender que devemos cuidadar até mesmo os mais pequenos detalhes!
 
 # Conclusão
-..
+Projeto divertido e simples, creio que seja importante todos desenvolvedores iniciantes praticarem com este projeto!
 
 [Resultado final]()
 [Link da publicação do LinkedIn]()
---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+import styles from '../styles/calculator.module.css'
+import react, { useState, useEffect } from 'react'
+
+export default function Home() {
+
+  const [calc, setCalc] = useState('')
+  const [result, setResult] = useState(0)
+
+  useEffect(() => {
+    try {
+      setResult(eval(calc))
+    } catch {
+      setResult(calc)
+    }
+  }, [calc])
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Calculadora | Ivan Führ</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.display}>
+          <input type="text" readOnly={true} value={calc} className="input" className={styles.operation} />
+          <input type="text" readOnly={true} value={result} className="input" className={styles.result} />
+
+        </div>
+        <div className={styles.buttons}>
+          <div className={styles.row}>
+            <button onClick={() => setCalc(calc + '(')}>(</button>
+            <button onClick={() => setCalc(calc + ')')}>)</button>
+            <button onClick={() => { setCalc(''); setResult(0) }}>AC</button>
+            <button onClick={() => setCalc(calc.slice(0, -1))}><i className="fa fa-eraser" aria-hidden="true"></i>
+            </button>
+          </div>
+          <div className={styles.row}>
+            <button onClick={() => setCalc(calc + '7')}>7</button>
+            <button onClick={() => setCalc(calc + '8')}>8</button>
+            <button onClick={() => setCalc(calc + '9')}>9</button>
+            <button onClick={() => setCalc(calc + '/')}>/</button>
+          </div>
+          <div className={styles.row}>
+            <button onClick={() => setCalc(calc + '4')}>4</button>
+            <button onClick={() => setCalc(calc + '5')}>5</button>
+            <button onClick={() => setCalc(calc + '6')}>6</button>
+            <button onClick={() => setCalc(calc + '*')}>*</button>
+          </div>
+          <div className={styles.row}>
+            <button onClick={() => setCalc(calc + '1')}>1</button>
+            <button onClick={() => setCalc(calc + '2')}>2</button>
+            <button onClick={() => setCalc(calc + '3')}>3</button>
+            <button onClick={() => setCalc(calc + '-')}>-</button>
+          </div>
+          <div className={styles.row}>
+            <button className={styles.zero} onClick={() => setCalc(calc + '0')}>0</button>
+            <button onClick={() => setCalc(calc + ',')}>,</button>
+            <button onClick={() => setCalc(calc + '+')}>+</button>
+          </div>
+          <div className={styles.row}>
+            <button className={styles.equals} onClick={() => setCalc(result)}>=</button>
+          </div>
+        </div>
+      </main>
+    </div >
+  )
+}
